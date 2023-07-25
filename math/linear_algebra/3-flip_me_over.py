41c643f1733e5c66b13dd0aa7035be29debcb7af
NMenendez24 <nahuelmt24@gmail.com>
2023-07-25 18:31:40 +0000

    pycodestyle

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,5 +5,5 @@
 def matrix_transpose(matrix):
     """Returns the transpose of a matrix"""
     new_matrix = [[matrix[i][j] for i in range(len(matrix))]
-                    for j in range(len(matrix[0]))]
+                  for j in range(len(matrix[0]))]
     return new_matrix
