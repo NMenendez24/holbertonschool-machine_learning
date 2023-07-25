ef80a3508ca223a8270dec2f825a7c1185258acc
NMenendez24 <nahuelmt24@gmail.com>
2023-07-25 18:17:02 +0000

    Permissions

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Shebang"""
 
+
 def matrix_transpose(matrix):
     """Returns the transpose of a matrix"""
     new_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
