6080c5281aeeb9828c90109d1355f8f64887ec76
NMenendez24 <nahuelmt24@gmail.com>
2023-07-26 17:02:41 +0000

    Pycodestyle

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,7 +6,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenate two matrices by axis"""
     new_matrix = []
     if axis == 0:
-        if len(mat1[0])!= len(mat2[0]):
+        if len(mat1[0]) != len(mat2[0]):
             return None
         new_matrix = mat1 + mat2
     if axis == 1:
