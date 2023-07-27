af3c349aa522893898625d4b600607e2d47fe869
NMenendez24 <nahuelmt24@gmail.com>
2023-07-27 19:10:23 +0000

    I Forgor

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,9 +2,6 @@
 """Shebang"""
 
 
-import numpy as np
-
-
 def np_elementwise(mat1, mat2):
     """Return a tuple with some calculations between matrices"""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
