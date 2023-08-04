b2032d267359edd8fe71381c01702d81628becc9
NMenendez24 <nahuelmt24@gmail.com>
2023-08-04 19:49:29 +0000

    a

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -10,7 +10,9 @@ def poly_integral(poly, C=0):
         return None
     new_poly = [C]
     for i in range(len(poly)):
-        if poly[i] != 0:
+        if i == len(poly) - 1 and poly[i] == 0:
+            continue
+        elif poly[i] != 0:
             result = poly[i] / (i + 1)
             new_poly.append(result if result % 1 else int(result))
         else:
