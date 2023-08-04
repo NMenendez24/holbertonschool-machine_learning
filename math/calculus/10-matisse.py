7cd8d4fb4d973f8a163006a107d6d62f4327d331
NMenendez24 <nahuelmt24@gmail.com>
2023-08-04 18:46:53 +0000

    Task 10 conditional Fix - Calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,10 +4,11 @@
 
 def poly_derivative(poly):
     """Calculate the derivative of a poly"""
-    if not isinstance(poly, list) or len(poly) < 2:
-        return None
     if isinstance(poly, list) and len(poly) == 1:
         return [0]
+    if not isinstance(poly, list) or len(poly) < 2:
+        return None
+    
     new_poly = []
     for x in range(1, len(poly)):
         new_poly.append(x * poly[x])
