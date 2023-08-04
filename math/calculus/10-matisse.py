7b6aefd89ed55d95a938dcaf01e164176e239588
NMenendez24 <nahuelmt24@gmail.com>
2023-08-04 18:44:26 +0000

    Task 9 and 10 Fix - Calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,9 +4,9 @@
 
 def poly_derivative(poly):
     """Calculate the derivative of a poly"""
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) < 2:
         return None
-    if len(poly) < 2:
+    if isinstance(poly, list) and len(poly) == 1:
         return [0]
     new_poly = []
     for x in range(1, len(poly)):
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """Sumation of the square"""
-    if not isinstance(n, int or float):
+    if not isinstance(n, int) or n < 1:
         return None
     return int((n * ((n + 1) * ((2 * n) + 1))) / 6)
