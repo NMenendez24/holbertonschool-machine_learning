e6c1b911a5ac41670630b64023cc40a3ea33f404
NMenendez24 <nahuelmt24@gmail.com>
2023-08-04 19:45:34 +0000

    Task 17 - Calculus

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Shebang"""
+
+
+def poly_integral(poly, C=0):
+    """Calculates the integral of a poly"""
+    if not isinstance(poly, list):
+        return None
+    if not isinstance(C, int) or len(poly) < 1:
+        return None
+    new_poly = [C]
+    for i in range(len(poly)):
+        if poly[i] != 0:
+            result = poly[i] / (i + 1)
+            new_poly.append(result if result % 1 else int(result))
+        else:
+            new_poly.append(0)
+    return new_poly
