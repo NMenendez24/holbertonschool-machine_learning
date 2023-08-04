34d905e0f838724800988ee77027744b8871aac7
NMenendez24 <nahuelmt24@gmail.com>
2023-08-04 18:39:41 +0000

    Task 9 without loop - Calculus

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,4 @@ def summation_i_squared(n):
     """Sumation of the square"""
     if not isinstance(n, int or float):
         return None
-    result = 0
-    for i in range(1, n + 1):
-        result = result + i ** 2
-    return result
+    return int((n * ((n + 1) * ((2 * n) + 1))) / 6)
