66f9377f7031ad114b1a5828431bbb853efd0959
NMenendez24 <nahuelmt24@gmail.com>
2023-08-16 14:54:41 +0000

    Fix error message

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -11,7 +11,7 @@ class Neuron:
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError('nx must be a positive number')
+            raise ValueError('nx must be a positive integer')
         self.b = 0
         self.A = 0
         self.W = np.random.normal(size=(1, nx))
--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -11,7 +11,7 @@ class Neuron:
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError('nx must be a positive number')
+            raise ValueError('nx must be a positive integer')
         self.__b = 0
         self.__A = 0
         self.__W = np.random.normal(size=(1, nx))
