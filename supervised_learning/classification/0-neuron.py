af12a903f42ecb579ae2813a812f562913ba61a9
NMenendez24 <nahuelmt24@gmail.com>
2023-08-16 14:41:31 +0000

    Task 0 - Classification

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Shebang"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Define a neuron"""
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive number')
+        self.b = 0
+        self.A = 0
+        self.W = np.random.normal(size=(1, nx))
