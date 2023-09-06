122be99d9ffe85d7094007e55f51f48fec9daf3b
NMenendez24 <nahuelmt24@gmail.com>
2023-09-06 16:04:01 +0000

    Task 18 - Classification

---
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Shebang"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Initializes a deep neural network"""
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(
+               layers) == 0 or min(layers) <= 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(1, self.__L + 1):
+            if i == 1:
+                self.__weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1], nx) * np.sqrt(2. / nx)
+            else:
+                self.__weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1], layers[i - 2]) * np.sqrt(2. / layers[i - 2])
+            self.__weights['b' + str(i)] = np.zeros((layers[i - 1], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    def sigmoid(self, Z):
+        """Sigmoid activation Function"""
+        return 1 / (1 + np.exp(-Z))
+
+    def forward_prop(self, X):
+        """Forward Propagation function"""
+        self.__cache["A0"] = X
+
+        for layer in range(self.__L):
+            w = "W{}".format(layer + 1)
+            b = "b{}".format(layer + 1)
+            a = "A{}".format(layer + 1)
+
+            z_1 = np.dot(self.__weights[w], self.__cache["A{}".format(layer)])
+            z = z_1 + self.__weights[b]
+            self.__cache[a] = self.sigmoid(z)
+        return self.__cache[a], self.__cache
